0001952cffdddb4d95c32473d25be133fae2c3ea
Dave Berkeley <via@rotwang.co.uk>
2021-07-11 15:38:00 +0100

    wip: first wkring with MQTT

--- psu_test.py
+++ psu_test.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 
+import sys
+import json
+import time
+
+from iot import broker3 as broker
+
+# Add the submodule to PATH
+sys.path.append('PyExpLabSys')
+sys.path.append('PyExpLabSys/PyExpLabSys/drivers')
+
 import tenma
+
+class Tenma722540(tenma.TenmaBase):
+    pass
+
+mqtt = broker.Broker("xively", server="mosquitto")
+
+topic = 'home/psu'
+
+#
+#
 
 def main():
     """Main module function, used for testing simple functional test"""
-    device = tenma.Tenma722540('/dev/psu')
+    device = Tenma722540('/dev/psu')
     print('ID:', device.get_identification())
     print('Status:', device.status())
 
@@ -20,13 +40,17 @@ def main():
     device.set_output(True)
 
     try:
-        for mv in range(0, 16000, 100):
+        for mv in range(0, 16500, 250):
             volts = mv / 1000.0
             device.set_voltage(volts)
-            #sleep(1)
+            #time.sleep(1)
             it = device.get_actual_current()
             vt = device.get_actual_voltage()
-            print(vt, it)
+            #print(vt, it)
+
+            msg = { 'V' : vt, 'I' : it }
+            mqtt.send(topic, json.dumps(msg))
+    
     except KeyboardInterrupt:
         pass
 
@@ -36,6 +60,32 @@ def main():
     device.set_overcurrent_protection(False)
     device.set_overvoltage_protection(False)
     device.recall_memory(1)
+
+    it = device.get_actual_current()
+    vt = device.get_actual_voltage()
+    print(vt, it)
+
+    msg = { 'V' : vt, 'I' : it }
+    mqtt.send(topic, json.dumps(msg))
+
+#
+#
+
+def monitor():
+    device = Tenma722540('/dev/psu')
+
+    while True:
+        try:
+            it = device.get_actual_current()
+            vt = device.get_actual_voltage()
+            #print(vt, it)
+    
+            msg = { 'V' : vt, 'I' : it }
+            mqtt.send(topic, json.dumps(msg))
+        except ValueError:
+            pass
+
+        time.sleep(1)
 
 main()
 
