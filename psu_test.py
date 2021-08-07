d9307a6ee50e52a4561ccb6de4c01ed1ec898a34
Dave Berkeley <via@rotwang.co.uk>
2021-08-07 10:53:53 +0100

    added arparse

--- psu_test.py
+++ psu_test.py
@@ -3,8 +3,8 @@
 import sys
 import json
 import time
-
-from iot import broker3 as broker
+import argparse
+import datetime
 
 # Add the submodule to PATH
 sys.path.append('PyExpLabSys')
@@ -12,138 +12,113 @@ sys.path.append('PyExpLabSys/PyExpLabSys/drivers')
 
 import tenma
 
+#
+#
+
 class Tenma722540(tenma.TenmaBase):
     pass
 
-mqtt = broker.Broker("xively", server="mosquitto")
-
-topic = 'home/psu'
-
 #
 #
 
-def discharge():
-    print('ID:', device.get_identification())
-    print('Status:', device.status())
+def monitor(mqtt, topic):
 
-    device.set_output(False)
-    device.recall_memory(5)
+    server = None
 
-    device.set_overcurrent_protection(True)
-    device.set_overvoltage_protection(True)
+    if mqtt and topic:
+        from iot import broker3 as broker
+        server = broker.Broker("tenma-psu", server=mqtt)
 
-    device.set_voltage(0.0)
-    device.set_current(2.0)
-    time.sleep(3)
-    device.set_output(True)
-
-    time.sleep(3)
-
-    try:
-        for mv in range(15000, 0, -10):
-            volts = mv / 1000.0
-            device.set_voltage(volts)
-            #time.sleep(1)
-            it = device.get_actual_current()
-            vt = device.get_actual_voltage()
-            #print(vt, it)
-
-            msg = { 'V' : vt, 'I' : it }
-            mqtt.send(topic, json.dumps(msg))
-    
-    except KeyboardInterrupt:
-        pass
-
-    print("Done")
-    device.set_output(False)
-    device.set_voltage(0.0)
-    device.set_overcurrent_protection(False)
-    device.set_overvoltage_protection(False)
-    device.recall_memory(1)
-
-    it = device.get_actual_current()
-    vt = device.get_actual_voltage()
-    print(vt, it)
-
-    msg = { 'V' : vt, 'I' : it }
-    mqtt.send(topic, json.dumps(msg))
-
-#
-#
-
-def monitor():
     while True:
         try:
             it = device.get_actual_current()
             vt = device.get_actual_voltage()
-            print(vt, it)
+            tt = datetime.datetime.now()
+            print(tt, vt, it)
  
-            msg = { 'V' : vt, 'I' : it }
-            mqtt.send(topic, json.dumps(msg))
+            if server:
+                msg = { 'V' : vt, 'I' : it }
+                server.send(topic, json.dumps(msg))
         except ValueError:
             pass
 
-        time.sleep(.1)
-
-#
-#
-
-def alert():
-    device.set_output(False)
-    device.recall_memory(5)
-
-    print('Overcurrent and overvoltage protection, watch the LEDS switch')
-    device.set_overcurrent_protection(True)
-    device.set_overvoltage_protection(True)
-
-    v = 16.0
-    device.set_voltage(v)
-    device.set_current(2.0)
-    device.set_output(True)
-
-    time.sleep(3)
-
-    it = device.get_actual_current()
-    vt = device.get_actual_voltage()
-    print(vt, it)
-
-    msg = { 'V' : vt, 'I' : it }
-    mqtt.send(topic, json.dumps(msg))
-
-    while True:
-        device.set_voltage(v)
-        v -= 0.01
-        time.sleep(0.1)
-
-        if v < 5:
-            break
-        
-        it = device.get_actual_current()
-        vt = device.get_actual_voltage()
-        print(vt, it)
-
-        msg = { 'V' : vt, 'I' : it }
-        mqtt.send(topic, json.dumps(msg))
-
+        time.sleep(0.01)
 
 #
 #
 
 if __name__ == "__main__":
-    device = Tenma722540('/dev/psu')
 
-    if len(sys.argv) > 1:
-        cmd = sys.argv[1]
-        if cmd == 'on':
-            device.set_voltage(16.8)
-            device.set_current(2.0)
-            device.set_output(True)
-        elif cmd == 'off':
-            device.set_output(False)
-    else:
-        #main()
-        monitor()
-        #discharge()
-        #alert()
+    p = argparse.ArgumentParser()
+    p.add_argument('--path', dest='path', default='/dev/psu', help='device path')
+    p.add_argument('commands', nargs='+')
+    p.add_argument('--max-v', dest='max_v', type=float, default=16.8)
+    p.add_argument('--max-i', dest='max_i', type=float, default=2.0)
+    p.add_argument('--overcurrent', dest='overcurrent', type=int, default=0)
+    p.add_argument('--overvoltage', dest='overvoltage', type=int, default=0)
+    p.add_argument('--on', dest='on', action='store_true', default=False)
+    p.add_argument('--mqtt', dest='mqtt', help='mqqt server')
+    p.add_argument('--topic', dest='topic', help='mqtt topic')
+
+    args = p.parse_args()
+    #print(args)
+
+    device = Tenma722540(args.path)
+
+    print('set overcurrent', args.overcurrent)
+    device.set_overcurrent_protection(args.overcurrent)
+    print('set overvoltage', args.overvoltage)
+    device.set_overvoltage_protection(args.overvoltage)
+
+    def set_voltage(vv):
+        assert (vv >= 0.0), vv
+        print(f'set v={vv}')
+        global v
+        assert(vv <= args.max_v), (vv, args.max_v)
+        v = vv
+        device.set_voltage(v)
+
+    i = device.get_actual_current()
+    v = device.get_actual_voltage()
+
+    if args.on:
+        print(f'switch on. v={v} i={i}')
+        device.set_output(True)        
+
+    repeat = False
+
+    while True:
+
+        for command in args.commands:
+            if command == 'on':
+                print(f'switch on. v={v} i={i}')
+                device.set_output(True)
+            elif command == 'off':
+                print('switch off')
+                device.set_output(False)
+            elif command.startswith('sleep='):
+                parts = command.split('=')
+                period = float(parts[1])
+                print('sleeping', period)
+                time.sleep(period)
+            elif command == 'repeat':
+                print('repeat')
+                repeat = True
+            elif command.startswith('v+'):
+                parts = command.split('+')
+                set_voltage(v + float(parts[1]))
+            elif command.startswith('v-'):
+                parts = command.split('-')
+                set_voltage(v - float(parts[1]))
+            elif command.startswith('v='):
+                parts = command.split('=')
+                set_voltage(float(parts[1]))
+            elif command == 'monitor':
+                monitor(args.mqtt, args.topic)
+            else:
+                raise Exception(f'Unknown command {command}')
+
+        if not repeat:
+            break
 
 # FIN
