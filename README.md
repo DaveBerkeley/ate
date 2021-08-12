b55dbea3ff936f13ab8c69b34ac0de2f97503465
Dave Berkeley <via@rotwang.co.uk>
2021-08-12 19:04:13 +0100

    added instructions for psu.py

--- README.md
+++ README.md
@@ -3,3 +3,56 @@ Automated Test Equipment
 
 Utilities to drive a Tenma 72-2540 bench supply over a serial port.
 
+Examples :
+
+select memory 5 : (note, the output is turned off by the psu when a memory is selected)
+
+    ./psu.py -m=5
+
+Overcurrent and overvoltage protection are enabled by default. Turn them off with the --overcurrent=0 or --overvoltage=0 options.
+
+Set the supply to 16V, turn it on :
+
+    ./psu.py v=16 on
+
+You can save to the memory locations :
+
+    ./psu.py m=3 v=12.0 i=2.0 save
+
+Turn the unit on to 5V for 10 seconds
+
+    ./psu.py v=5 on sleep=10 off
+
+Turn the unit off for 2s, using the existing settings :
+
+    ./psu.py off sleep=2 on
+
+It has a very simple loop construct. You mark the start of the loop with 'loop' (defaults to the first command) and repeat on 'repeat'.
+
+So, to set the supply to 5V and ramp down to 0V, do :
+
+    ./psu.py v=5 on loop sleep=1 v-0.1 repeat
+
+This will bail out when V reaches 0.0 by default, or you can set the minimum with eg. the --min-v=2.0 command.
+
+The loop will also bail if it reaches the upper voltage limit set by --max-v=V
+
+    ./psu.py --max-v=10 v=0 on loop sleep=1 v+0.5 repeat
+
+You can continuously output the status using the 'monitor' command :
+
+    ./psu.py monitor
+
+All the commands print info starting with a ':', except the 'show' command, which prints time, voltage and current. This can be used to dump the data to a file, or plot it in real time.
+
+    ./psu.py --overvoltage=0 --max-v=10 v=0 on loop sleep=0.1 show v+0.5 repeat | grep -v "^:"
+
+There is an MQTT client available which sends JSON data to a named server / topic. This applies to the 'show' and 'monitor' commands.
+
+    ./psu.py --mqtt server --topic ate/psu/0 monitor
+
+The JSON data looks like this :
+
+    {"V": 5.0, "I": 0.0}
+
+FIN
--- psu.py
+++ psu.py
@@ -23,28 +23,25 @@ class Tenma722540(tenma.TenmaBase):
 #
 #
 
-def monitor(mqtt, topic):
+def show(server, topic, i, v):
+    tt = datetime.datetime.now()
+    print(tt, v, i)
 
-    server = None
+    if server and topic:
+        msg = { 'V' : v, 'I' : i }
+        server.send(topic, json.dumps(msg))
+#
+#
 
-    if mqtt and topic:
-        from iot import broker3 as broker
-        server = broker.Broker("tenma-psu", server=mqtt)
+def monitor(server, topic):
 
     while True:
         try:
             it = device.get_actual_current()
             vt = device.get_actual_voltage()
-            tt = datetime.datetime.now()
-            print(tt, vt, it)
- 
-            if server:
-                msg = { 'V' : vt, 'I' : it }
-                server.send(topic, json.dumps(msg))
+            show(server, topic, it, vt)
         except ValueError:
             pass
-
-        time.sleep(0.01)
 
 #
 #
@@ -56,6 +53,7 @@ if __name__ == "__main__":
     p.add_argument('commands', nargs='+')
     p.add_argument('--max-v', dest='max_v', type=float, default=16.8)
     p.add_argument('--max-i', dest='max_i', type=float, default=2.0)
+    p.add_argument('--min-v', dest='min_v', type=float, default=0.0)
     p.add_argument('--overcurrent', dest='overcurrent', type=int, default=1)
     p.add_argument('--overvoltage', dest='overvoltage', type=int, default=1)
     p.add_argument('--on', dest='on', action='store_true', default=False)
@@ -69,31 +67,34 @@ if __name__ == "__main__":
     device = Tenma722540(args.path)
 
     memory = args.memory
-    #print(f'set memory={memory}')
-    #device.recall_memory(memory)
 
-    print('set overcurrent', args.overcurrent)
+    server = None
+    if args.mqtt and args.topic:
+        from iot import broker3 as broker
+        server = broker.Broker("tenma-psu", server=args.mqtt)
+
+    print(':set overcurrent', args.overcurrent)
     device.set_overcurrent_protection(args.overcurrent)
-    print('set overvoltage', args.overvoltage)
+    print(':set overvoltage', args.overvoltage)
     device.set_overvoltage_protection(args.overvoltage)
 
     def set_voltage(vv):
-        assert (vv >= 0.0), vv
-        print(f'set v={vv}')
+        assert(vv >= args.min_v), (vv, args.min_v)
         global v
         assert(vv <= args.max_v), (vv, args.max_v)
         v = vv
+        print(f':set v={v}')
         device.set_voltage(v)
 
     i = args.max_i
-    print(f'set current={i}')
+    print(f':set current={i}')
     device.set_current(i)
 
     i = device.get_actual_current()
     v = device.get_actual_voltage()
 
     if args.on:
-        print(f'switch on. v={v} i={i}')
+        print(f':switch on. v={v} i={i}')
         device.set_output(True)  
 
     repeat = False
@@ -101,48 +102,60 @@ if __name__ == "__main__":
     idx = 0
     loop = 0
 
-    while idx < len(args.commands):
+    try:
+        while idx < len(args.commands):
 
-        command = args.commands[idx]
-        idx += 1
+            sys.stdout.flush()
 
-        if command == 'on':
-            print(f'switch on. v={v} i={i}')
-            device.set_output(True)
-        elif command == 'off':
-            print('switch off')
-            device.set_output(False)
-        elif command.startswith('sleep='):
-            parts = command.split('=')
-            period = float(parts[1])
-            print('sleeping', period)
-            time.sleep(period)
-        elif command == 'repeat':
-            print('repeat', loop)
-            idx = loop
-        elif command == 'loop':
-            print('loop')
-            loop = idx
-        elif command.startswith('v+'):
-            parts = command.split('+')
-            set_voltage(v + float(parts[1]))
-        elif command.startswith('v-'):
-            parts = command.split('-')
-            set_voltage(v - float(parts[1]))
-        elif command.startswith('v='):
-            parts = command.split('=')
-            set_voltage(float(parts[1]))
-        elif command.startswith('m='):
-            parts = command.split('=')
-            memory = int(parts[1])
-            print(f'set memory={memory}')
-            device.recall_memory(memory)
-        elif command == 'monitor':
-            monitor(args.mqtt, args.topic)
-        elif command == 'save':
-            print('save memory', memory)
-            device.save_memory(memory)
-        else:
-            raise Exception(f'Unknown command {command}')
+            command = args.commands[idx]
+            idx += 1
+
+            if command == 'on':
+                print(f':switch on. v={v} i={i}')
+                device.set_output(True)
+            elif command == 'off':
+                print(':switch off')
+                device.set_output(False)
+            elif command.startswith('sleep='):
+                parts = command.split('=')
+                period = float(parts[1])
+                print(':sleeping', period)
+                time.sleep(period)
+            elif command == 'repeat':
+                print(':repeat')
+                idx = loop
+            elif command == 'loop':
+                print(':loop')
+                loop = idx
+            elif command.startswith('v+'):
+                parts = command.split('+')
+                set_voltage(v + float(parts[1]))
+            elif command.startswith('v-'):
+                parts = command.split('-')
+                set_voltage(v - float(parts[1]))
+            elif command.startswith('v='):
+                parts = command.split('=')
+                set_voltage(float(parts[1]))
+            elif command.startswith('i='):
+                parts = command.split('=')
+                device.set_current(float(parts[1]))
+            elif command.startswith('m='):
+                parts = command.split('=')
+                memory = int(parts[1])
+                print(f':set memory={memory}')
+                device.recall_memory(memory)
+            elif command == 'monitor':
+                monitor(server, args.topic)
+            elif command == 'show':
+                it = device.get_actual_current()
+                vt = device.get_actual_voltage()
+                show(server, args.topic, it, vt)
+            elif command == 'save':
+                print(':save memory', memory)
+                device.save_memory(memory)
+            else:
+                raise Exception(f'Unknown command {command}')
+    except AssertionError as ex:
+        print(':AssertionError', ex)
 
 # FIN
